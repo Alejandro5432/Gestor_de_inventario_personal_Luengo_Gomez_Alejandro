4cbc7007e8fde2f9e67cf75985870c06c070b0d2
Alejandro5432 <alejandro.luegom@educa.jcyl.es>
2025-11-12 10:01:58 +0100

    Push inicial

---
+++ app/src/test/java/es/ies/claudiomoyano/dam2/pmdm/gestor_de_inventario_personal_luengo_gomez_alejandro/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package es.ies.claudiomoyano.dam2.pmdm.gestor_de_inventario_personal_luengo_gomez_alejandro;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
