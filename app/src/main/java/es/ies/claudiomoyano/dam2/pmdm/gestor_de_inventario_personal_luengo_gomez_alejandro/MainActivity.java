79253293d5c11afd6721e38d58d1ecdb6b6d9bae
Alejandro5432 <alejandro.luegom@educa.jcyl.es>
2025-11-13 11:06:56 +0100

    Push del segundo día

--- app/src/main/java/es/ies/claudiomoyano/dam2/pmdm/gestor_de_inventario_personal_luengo_gomez_alejandro/MainActivity.java
+++ app/src/main/java/es/ies/claudiomoyano/dam2/pmdm/gestor_de_inventario_personal_luengo_gomez_alejandro/MainActivity.java
@@ -1,11 +1,19 @@
 package es.ies.claudiomoyano.dam2.pmdm.gestor_de_inventario_personal_luengo_gomez_alejandro;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    TextView tvFechSeleccionada;
+    int dia, mes, anio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +21,33 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        // tvFechaSeleccionada está en el layout pertenencia_card_layout, por eso da error
+        tvFechSeleccionada = findViewById(R.id.tvFechaSeleccionada);
 
+        // Muestro un DatePicker cuando se haga clic en el TextView y almaceno ahí la fecha elegida
+        tvFechSeleccionada.setOnClickListener(v-> mostrarDatePicker());
+    }
+
+    private void mostrarDatePicker(){
+
+        final Calendar calendario = Calendar.getInstance();
+        anio = calendario.get(Calendar.YEAR);
+        mes = calendario.get(Calendar.MONTH);
+        dia = calendario.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                (view, year, month, dayOfMonth) -> {
+            // Actualizo el TextView con la fecha elegida
+            String fecha = dayOfMonth+"/"+(month+1)+"/"+year;
+            tvFechSeleccionada.setText(fecha);
+
+            /* Esto se podría usar para guardar la fecha en una variable
+            anio = year;
+            mes = month;
+            dia = dayOfMonth;*/
+
+        }, anio, mes, dia);
+        // Por último lo muestro:
+        datePickerDialog.show();
     }
 }
--- app/src/main/java/es/ies/claudiomoyano/dam2/pmdm/gestor_de_inventario_personal_luengo_gomez_alejandro/PertenenciaAdapter.java
+++ app/src/main/java/es/ies/claudiomoyano/dam2/pmdm/gestor_de_inventario_personal_luengo_gomez_alejandro/PertenenciaAdapter.java
@@ -1,4 +1,40 @@
 package es.ies.claudiomoyano.dam2.pmdm.gestor_de_inventario_personal_luengo_gomez_alejandro;
 
-public class PertenenciaAdapter {
+import android.content.Context;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class PertenenciaAdapter extends RecyclerView.Adapter<PertenenciaAdapter.> {
+    private Context contexto;
+    private List<Pertenencia> listaPertenencias;
+
+    public PertenenciaAdapter(Context contexto, List<Pertenencia> listaPertenencias){
+        this.contexto = contexto;
+        this.listaPertenencias = listaPertenencias;
+    }
+
+
 }
+
+    public static class PertenenciaViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView textViewNombre;
+        public TextView textViewCategoria;
+        public TextView textViewUnidades;
+        public TextView textViewPeso;
+        public TextView textViewDimensiones;
+        public TextView textViewFragil;
+        public TextView textViewPrecio;
+        public TextView textViewFechsCompra;
+
+        public PertenenciaViewHolder(@NonNull View itemView, final AdapterView.OnItemClickListener listener){
+            super(itemView);
+
+        }
+    }
